3c1f3012eb069d2012fc1ccd9bd3a70fd3e17391
navjeetgill <navjeetgill240297@gmail.com>
2021-07-10 17:38:25 +0530

    new commit

---
+++ start with tera/createinstance.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key = "AKIA5ZRJOLOFR5ZQX7GK"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-2"
+}
+
+resource "aws_instance" "MyFirstInstnace" {
+  count         = 3
+  ami           = "ami-0d563aeddd4be7fff"
+  instance_type = "t2.micro"
+}
