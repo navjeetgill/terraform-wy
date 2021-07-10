b30a1dc8688e5e2916091a336481a3ebec9c8539
navjeetgill <navjeetgill240297@gmail.com>
2021-07-10 17:56:35 +0530

    new chng

--- start with teraform  basics/createinstance.tf
+++ start with teraform  basics/createinstance.tf
@@ -4,8 +4,7 @@ provider "aws" {
   region     = "us-east-2"
 }
 
-resource "aws_instance" "MyFirstInstnace" {
-  count         = 3
+resource "aws_instance" "MyFirstInstnace" {  
   ami           = "ami-0d563aeddd4be7fff"
   instance_type = "t2.micro"
 }
